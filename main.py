c66a63dd4242b884af5194273279823b4d47ec82
mxgrph <kriksisu5050@gmail.com>
2020-01-24 07:44:51 +0000

    added params

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -12,11 +12,14 @@ def home():
 
 @app.route('/about')
 def about():
-  return render_template('about.html')
+  return render_template('about.html', active_page = 'about')
 
 @app.route('/contact')
 def contact():
-  return render_template('contact.html', phone = 696969)
+  return render_template('contact.html', phone = "1111111")
 
+@app.route('/params')
+def params():
+  return render_template('params.html', args = request.args.to_dict())
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', port = 5211, threaded = True, debug = True)
