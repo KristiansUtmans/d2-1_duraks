4a01af322130bb34e7dfe93e6f1d98366624cddf
Kristians <56531050+mxgrph@users.noreply.github.com>
2020-02-03 14:13:01 +0200

    JSON testing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from file_proc import write_file, read_file
 
 app = Flask(__name__)
 
@@ -26,9 +27,27 @@ def params():
 def post():
   return request.get_json()
 
+@app.route('/read_from_file')
+def readFromFile():
+  content = read_file()
+  return content
 
+@app.route('/write_to_file', methods = ['POST'])
+def writeToFile():
+  request_type = request.content_type
+  if (request_type == "application/json"):
+    contentJSON = request.get_json()
+    write_file(contentJSON['data'])
+    return contentJSON
+  else:
+    return f"Request type: '{request_type} not supported!"
 
-
+@app.route('/get_file', methods = ['POST', 'GET'])
+def get_file():
+  if request.method == 'GET':
+    return readFromFile()
+  elif request.method == 'POST':
+    return writeToFile()
 
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', port = 5211, threaded = True, debug = True)
